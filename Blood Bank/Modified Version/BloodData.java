ac52a98b3cf93453a2d45bd8e71bd8f1805009de
Genivieve <subito.253227@pasay-edsa.sti.edu.ph>
2024-11-04 23:25:33 +0800

    Create BloodData.java

---
+++ Blood Bank/Modified Version/BloodData.java
@@ -0,0 +1,29 @@
+    class BloodData {
+        private String bloodType;
+        private String rhFactor;
+
+        public BloodData() {
+            this.bloodType = "O";
+            this.rhFactor = "+";
+        }
+
+        public void setBloodType(String bloodType) {
+            this.bloodType = bloodType;
+        }
+
+        public void setRhFactor(String rhFactor) {
+            this.rhFactor = rhFactor;
+        }
+
+        public String getBloodType() {
+            return bloodType;
+        }
+
+        public String getRhFactor() {
+            return rhFactor;
+        }
+
+        public void display() {
+            System.out.println("Blood Type: " + getBloodType() + ", Rhesus Factor: " + getRhFactor());
+        }
+    }
