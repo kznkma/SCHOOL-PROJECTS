e63ae49bc4ab5eed7fa89832038d38b353ab72f4
Genivieve <subito.253227@pasay-edsa.sti.edu.ph>
2024-11-04 23:44:10 +0800

    Create Person.java

---
+++ College List/Person.java
@@ -0,0 +1,20 @@
+class Person {
+    private String name;
+    private String contactNum;
+
+    public void setName(String n) {
+        this.name = n;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setContactNum(String c) {
+        this.contactNum = c;
+    }
+
+    public String getContactNum() {
+        return this.contactNum;
+    }
+}
