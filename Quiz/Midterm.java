d0b6fe8edbab0827a77a079f0cc3c08d4d317895
Genivieve <subito.253227@pasay-edsa.sti.edu.ph>
2024-11-05 00:02:24 +0800

    Create Midterm.java

---
+++ Quiz/Midterm.java
@@ -0,0 +1,7 @@
+public class Midterm extends Exam {
+    // Constructor that prints "Exam has started."
+    public Midterm() {
+        super(); // Call to parent class constructor
+        System.out.println("Exam has started.");
+    }
+}
