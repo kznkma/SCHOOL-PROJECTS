7e80482c79918cccf3e8b22f64436d4f8b64040b
Genivieve <subito.253227@pasay-edsa.sti.edu.ph>
2024-11-18 22:23:05 +0800

    Create PlayerImpl.java

---
+++ Text-Based Game (with logic)/PlayerImpl.java
@@ -0,0 +1,13 @@
+public class PlayerImpl implements Player {
+    private String name;
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
